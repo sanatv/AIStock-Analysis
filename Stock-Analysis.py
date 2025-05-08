a7cc8b32d902517d494626306e2980076e5943bf
sanatv <34750267+sanatv@users.noreply.github.com>
2025-05-08 18:21:56 -0500

    Update Stock-Analysis.py

--- Stock-Analysis.py
+++ Stock-Analysis.py
@@ -619,7 +619,7 @@ with tabs[0]:
                 line=dict(color="rgba(80,260,0,100)", width=1),
                 label=labels,
                 color=node_colors,
-                hoverlabel=dict(bgcolor="rgba(80,60,256,80)", font=dict(color="white"))
+                hoverlabel=dict(bgcolor="rgba(80,60,256,80)", font=dict(color="black"))
             ),
             link=dict(
                 source=source,
